555ec83a10b696890823712bddc69b0749507d78
Anhad Singh <anhad922@gmail.com>
2021-10-28 21:01:52 +0530

    Data Models

---
+++ backend/models/productModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    manufacturer: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    minQuantity: {
+      type: Number,
+    },
+    maxQuantity: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
